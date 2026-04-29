490c0eed25c2913838a009a20bd181175388c512
Siddu Nhavi <siddunhavi@gmail.com>
2026-04-29 13:20:22 +0530

    server is set to run

---
+++ backend/App.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the TradeNexus API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});     
+
+
