4a806a4ca6eae2cf95d2605467230a90badcc246
Siddu Nhavi <siddunhavi@gmail.com>
2026-05-07 02:02:57 +0530

    fixed signup message func

--- frontend/src/landing_page/signup/SignUp.jsx
+++ frontend/src/landing_page/signup/SignUp.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function SignUp() {
-    return ( 
-        <>
-            <h1>Signup into account</h1>
-        </>
-     );
+    return (
+        <h1>Signup</h1>
+    );
 }
 
 export default SignUp;
