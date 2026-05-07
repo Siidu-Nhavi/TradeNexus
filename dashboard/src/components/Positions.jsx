6c89eacfd15c27d31d237ca40e7962b2bc9d502e
Siddu Nhavi <siddunhavi@gmail.com>
2026-05-07 23:21:22 +0530

    dashboard is setup is done

---
+++ dashboard/src/components/Positions.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Positions(){
+  return (
+    <>
+      <h3 className="title">Positions (2)</h3>
+
+      <div className="order-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Instrument</th>
+              <th>Qty.</th>
+              <th>Avg.</th>
+              <th>LTP</th>
+              <th>P&L</th>
+              <th>Chg.</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Positions;
