71c3f37b8a93b037101039632e877b064da173a0
Siddu Nhavi <siddunhavi@gmail.com>
2026-05-10 22:30:04 +0530

    Holdings and Pricing data is loaded from database

--- dashboard/src/components/Positions.jsx
+++ dashboard/src/components/Positions.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect, use } from 'react';
+
 
 function Positions(){
+  const [allPostions,setAllPostions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/allPositions")
+      .then((response) => response.json())
+      .then((data) => setAllPostions(data))
+      .catch((error) => console.error("Error fetching positions:", error));
+  }, []);
   return (
     <>
       <h3 className="title">Positions (2)</h3>
@@ -18,6 +27,30 @@ function Positions(){
               <th>Chg.</th>
             </tr>
           </thead>
+          <tbody>
+            {allPostions.map((stock, index) => {
+              const qty = Number(stock.qty) || 0;
+              const avg = Number(stock.avg) || 0;
+              const price = Number(stock.price) || 0;
+              const curValue = price * qty;
+              const profit = curValue - avg * qty;
+              const isProfit = profit >= 0;
+              const profClass = isProfit ? "profit" : "loss";
+              const dayClass = stock.isLoss ? "loss" : "profit";
+
+              return (
+                <tr key={index}>
+                  <td>{stock.product}</td>
+                  <td>{stock.name}</td>
+                  <td>{qty}</td>
+                  <td>{avg.toFixed(2)}</td>
+                  <td>{price.toFixed(2)}</td>
+                  <td className={profClass}>{profit.toFixed(2)}</td>
+                  <td className={dayClass}>{stock.day}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </>
